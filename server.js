b428f654f8ff07df8ecc720adf37287bf9d6142a
unknown <ethanmstrain@gmail.com>
2023-02-18 19:53:38 -0700

    search functioning

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '25mb', extended: false}));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://RIPVANWINKLE:plop@cluster0.rvosq.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true})
@@ -15,5 +19,6 @@ mongoose.connect('mongodb+srv://RIPVANWINKLE:plop@cluster0.rvosq.mongodb.net/?re
 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
